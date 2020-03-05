d9ff82981aa8ffd36e34b617fcc828a468aa1343
James <mrjames8124@gmail.com>
2020-03-05 13:49:50 -0800

    creating ajax request

--- client/main.js
+++ client/main.js
@@ -1,7 +1,23 @@
-$(document).ready(function () {
+$(document).ready(initalizeApp);
+
+function initalizeApp() {
   $('.your-class').slick({
     infinite: true,
     slidesToShow: 6,
     slidesToScroll: 6
-      });
+  });
+  getSchedule();
+}
+
+
+
+  function getSchedule(){
+    $.ajax({
+      dataType: "json",
+      url: "/schedule?country=:countrycode&date=:date",
+      method: "GET",
+      success: function (result) {
+        $("#div1").html(result);
+      },
     });
+  }
