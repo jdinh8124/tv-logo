cb304fc3fa9abbfc919a45679433be7617704932
James <mrjames8124@gmail.com>
2020-03-06 15:51:24 -0800

    iphone x completed

--- client/main.js
+++ client/main.js
@@ -14,25 +14,26 @@ function initalizeApp() {
 
 
   function getSchedule(){
-    let currentDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
-    let day = currentDate.getDate()
-    let month = currentDate.getMonth() + 1
-    console.log(month)
-    if(month < 10){
-      month = "0" + month;
-    }
-    if (day < 10) {
-      day = "0" + day;
-    }
-    let year = currentDate.getFullYear()
-    let date = `${year}-${month}-${day}`;
-
+    // let currentDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+    // let day = currentDate.getDate()
+    // let month = currentDate.getMonth() + 1
+    // console.log(month)
+    // if(month < 10){
+    //   month = "0" + month;
+    // }
+    // if (day < 10) {
+    //   day = "0" + day;
+    // }
+    // let year = currentDate.getFullYear()
+    // let date = `${year}-${month}-${day}`;
+    let date = "2020-03-06"
     console.log(date)
     $.ajax({
       dataType: "json",
       url: `http://api.tvmaze.com/schedule?country=US&date=${date}`,
       method: "GET",
       success: function (result) {
+        console.log(result)
         for(let i = 0; i < 25; i++){
           let image = result[i].show.image.medium;
           let altDescription = result[i].show.name;
