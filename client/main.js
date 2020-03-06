56dc41e1863de9b4c0863becd42d158237d673dc
James <mrjames8124@gmail.com>
2020-03-05 19:34:24 -0800

    full screen comparable to template design

--- client/main.js
+++ client/main.js
@@ -7,6 +7,32 @@ function initalizeApp() {
     dots:false,
     slidesToShow: 6,
     slidesToScroll: 6,
+    accessibility: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 }
 
