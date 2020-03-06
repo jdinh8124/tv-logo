f66ba4cf536f9caaa00ff96990d2c693b931d600
James <mrjames8124@gmail.com>
2020-03-05 18:35:46 -0800

    slider completed

--- client/main.js
+++ client/main.js
@@ -1,16 +1,14 @@
 $(document).ready(initalizeApp);
 
 function initalizeApp() {
-  $('#slide .slick-slider').slick({
-    infinite: true,
-    arrows: true,
-    dots:true,
-    slidesToShow: 6,
-    slidesToScroll: 6
-  });
   getSchedule();
+  $('.slick-slider').slick({
+    arrows: true,
+    dots:false,
+    slidesToShow: 6,
+    slidesToScroll: 6,
+  });
 }
-
 
 
   function getSchedule(){
@@ -20,11 +18,12 @@ function initalizeApp() {
       url: `http://api.tvmaze.com/schedule?country=US&date=${date}`,
       method: "GET",
       success: function (result) {
-        for(let i = 0; i < 26; i++){
+        for(let i = 0; i < 25; i++){
           let image = result[i].show.image.medium;
           let altDescription = result[i].show.name;
-          let picture = $("<img>").attr("src", image).attr("alt", altDescription).addClass("tmr-tv-icon")
-          $('.slick-slider').append(picture);
+          let picture = $("<img>").attr("src", image).attr("alt", altDescription).addClass('show-pic')
+          let divToAppendPic = `.show${i}`;
+          $(divToAppendPic).append(picture);
        }
      },
    });
