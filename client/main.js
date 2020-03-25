bf16f84d07139bdf2d1aaee100d8e23ee8e56cd8
James <mrjames8124@gmail.com>
2020-03-25 11:41:38 -0700

    removed all margin from show pic

--- client/main.js
+++ client/main.js
@@ -35,7 +35,18 @@ function initalizeApp() {
           dots: false,
           slidesToShow: 6,
           slidesToScroll: 6,
-          accessibility: true
+          accessibility: true,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                infinite: true,
+                dots: true
+              }
+            }
+          ]
         });
     },
   });
